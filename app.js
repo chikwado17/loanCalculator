a46733328efdaaa721410883b148b4eb970608a7
chikwado17 <chikwadonworie@gmail.com>
2020-11-10 10:58:20 +0100

    added the loan calculator formula

---
+++ app.js
@@ -0,0 +1,43 @@
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+
+function calculateResults(e) {
+    e.preventDefault();
+    
+    const amount = document.getElementById('amount')
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    //the calculation formula
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+
+    //compute monthly payment
+    //solution
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x-1);
+
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        
+    }else {
+        const alert = document.getElementById('alert'); 
+        alert.style.display = 'block'; 
+    }
+  
+}
+
+function hideDiv() {
+    const alert = document.getElementById('alert'); 
+    alert.style.display = 'none'; 
+}
+setTimeout("hideDiv()",5000);
